baf16119997552ff05443eca8280d1fcefdf54c7
Laszlo Somossy <71734784+StonyDigi@users.noreply.github.com>
2024-03-04 20:13:32 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const amountOne = document.querySelector('.amount-1');
+const amountTwo = document.querySelector('.amount-2');
+const amountThree = document.querySelector('.amount-3');
+const amountFour = document.querySelector('.amount-4');
+const amountFive = document.querySelector('.amount-5');
+const amountSix = document.querySelector('.amount-6');
+const amountSeven = document.querySelector('.amount-7');
+
+document.querySelector(".bar-1").addEventListener('mouseover', () => {
+   amountOne.style.visibility = 'visible';
+});
+
+document.querySelector(".bar-1").addEventListener('mouseout', () => {
+    amountOne.style.visibility = 'hidden';
+});
+
+document.querySelector(".bar-2").addEventListener('mouseover', () => {
+    amountTwo.style.visibility = 'visible';
+});
+
+document.querySelector(".bar-2").addEventListener('mouseout', () => {
+    amountTwo.style.visibility = 'hidden';
+});
+
+document.querySelector(".bar-3").addEventListener('mouseover', () => {
+    amountThree.style.visibility = 'visible';
+});
+
+document.querySelector(".bar-3").addEventListener('mouseout', () => {
+    amountThree.style.visibility = 'hidden';
+});
+
+document.querySelector(".bar-4").addEventListener('mouseover', () => {
+    amountFour.style.visibility = 'visible';
+});
+
+document.querySelector(".bar-4").addEventListener('mouseout', () => {
+    amountFour.style.visibility = 'hidden';
+});
+
+document.querySelector(".bar-5").addEventListener('mouseover', () => {
+    amountFive.style.visibility = 'visible';
+});
+
+document.querySelector(".bar-5").addEventListener('mouseout', () => {
+    amountFive.style.visibility = 'hidden';
+});
+
+document.querySelector(".bar-6").addEventListener('mouseover', () => {
+    amountSix.style.visibility = 'visible';
+});
+
+document.querySelector(".bar-6").addEventListener('mouseout', () => {
+    amountSix.style.visibility = 'hidden';
+});
+
+document.querySelector(".bar-7").addEventListener('mouseover', () => {
+    amountSeven.style.visibility = 'visible';
+});
+
+document.querySelector(".bar-7").addEventListener('mouseout', () => {
+    amountSeven.style.visibility = 'hidden';
+});
+
+
